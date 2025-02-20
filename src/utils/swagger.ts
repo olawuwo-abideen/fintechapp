50c44373e2b166f619a7797d023e5f89a9807ee8
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2025-02-20 06:59:15 +0100

    update

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,3 +1,51 @@
+// import swaggerJsDoc from 'swagger-jsdoc';
+// import swaggerUi from 'swagger-ui-express';
+// import { Express } from 'express';
+
+// const options: swaggerJsDoc.Options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Fintech API',
+//       version: '1.0.0',
+//       description: 'Backend to a Fintech API documentation',
+//     },
+//     servers: [
+//       {
+//         url: 'http://localhost:3000',
+//         description: 'Local server',
+//       },
+//     ],
+//     components: {
+//       securitySchemes: {
+//         BearerAuth: {
+//           type: 'http',
+//           scheme: 'bearer',
+//           bearerFormat: 'JWT',
+//         },
+//       },
+//     },    
+//     // security: [{ BearerAuth: [] }],
+//   },
+//   apis: ['./src/routers/*.ts']
+// };
+
+// const swaggerSpec = swaggerJsDoc(options);
+
+// export const swaggerDocs = (app: Express): void => {
+//   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec,
+//     {
+//       swaggerOptions: {
+//         persistAuthorization: true
+//       },
+//     }
+//   ));
+// };
+
+
+
+
+
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
@@ -24,20 +72,15 @@ const options: swaggerJsDoc.Options = {
           bearerFormat: 'JWT',
         },
       },
-    },    
-    // security: [{ BearerAuth: [] }],
+    },
   },
-  apis: ['./src/routers/*.ts']
+  apis: ['./src/routers/*.ts'],
 };
 
 const swaggerSpec = swaggerJsDoc(options);
 
 export const swaggerDocs = (app: Express): void => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec,
-    {
-      swaggerOptions: {
-        persistAuthorization: true
-      },
-    }
-  ));
+  app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    swaggerOptions: { persistAuthorization: true },
+  }));
 };
